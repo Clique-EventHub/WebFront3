2f9a05592a6654ca4ed1740011eba890d66a78bd
focusjk <jiraphat.khupanit@gmail.com>
2017-06-28 15:08:41 +0700

    no message

--- src/container/channelInfo.js
+++ src/container/channelInfo.js
@@ -1,205 +1,229 @@
 import React, { Component } from 'react';
-import './css/eventDetail2.css'
-import * as facultyMap from '../actions/facultyMap';
-import axios from 'axios';
-import { hostname } from '../actions/index';
-import ReactLoading from 'react-loading';
+import autoBind from '../hoc/autoBind';
+import EventItem from '../container/eventItem';
+import './css/editEvent2.css';
 import './css/channelInfostyle.css';
+import axios from 'axios';
+import { getCookie } from '../actions/common';
 
-//https://codepen.io/bh/pen/JBlCc
-let useCls = " toggle-vis";
-const codeList = facultyMap.getCodeList();
-let index = 0;
-
-const defaultState = {
-    'title': null,
-    'about': null,
-    'video': null,
-    'channel': null,
-    'location': null,
-    'date_start': null,
-    'expire': null,
-    'date_end': null,
-    'picture': null,
-    'picture_large': null,
-    'year_require': null,
-    'faculty_require': null,
-    'tags': null,
-    'forms': null,
-    'isLoading': true,
-    'error': null
-}
-
-class channelInfo extends Component {
-
+class EditEvent extends Component {
     constructor(props) {
         super(props);
-        this.onBtnClick = this.onBtnClick.bind(this);
-        this.onResetPopup = this.onResetPopup.bind(this);
-        this.onGetInfo = this.onGetInfo.bind(this);
-        this.state = defaultState;
-    }
 
-    onExit() {
-        this.props.onToggle();
-    }
+        // title,about,video,channel,location,date_start,expire,date_end,picture,picture_large, year_require,faculty_require,tags,forms
 
-    componentDidMount() {
-        this.onResetPopup();
-        this.onGetInfo();
-    }
+        // about, video, location, date_start, date_end, picture, picture_large, year_require, faculty_require, tags, agreement, contact_information,
+        // joinable_start_time, joinable_end_time, joinable_amount, time_start, time_end, optional_field, require_field, show, outsider_accessible
 
-    componentDidUpdate() {
-        console.log(this.state);
-    }
+        let _this = this;
 
-    onGetInfo() {
-        this.setState({
-            ...this.state,
-            'isLoading': true
-        });
-        axios.get(`${hostname}event?id=${this.props.eventId}`).then((data) => {
-            let rObj = {
-                ...defaultState,
-                ...data.data,
-                isLoading: false
-            }
-            this.setState(rObj);
-            this.onResetPopup();
-            return data.data;
-        }).then((event) => {
-            axios.get(`${hostname}channel?id=${event.channel}&stat=false`).then((res) => {
-                this.setState({
-                    ...this.state,
-                    'channel': res.data
-                });
-                this.onResetPopup();
-            })
-        }).catch((error) => {
-            this.setState({
-                ...this.state,
-                'error': 'Oh! Ow! Something went wrong. Please check your internet connection',
-                'isLoading': false
-            });
-        })
-    }
-
-    onResetPopup() {
-        Object.keys(this.refs).forEach((key) => {
-            if(!this.refs[key].className.includes(useCls)) this.refs[key].className += useCls;
-        })
-    }
-
-    onBtnClick(refName) {
-        if(this.refs[refName].className.includes(useCls)) {
-            this.onResetPopup();
-            this.refs[refName].className = this.refs[refName].className.replace(useCls, "");
-        } else {
-            this.refs[refName].className += useCls;
+        this.state = {
+            'event_id': "594bf476e374d100140f04ec"
         }
+
+        axios.get('http://128.199.208.0:1111/event?id=' + _this.state.event_id).then((data) => {
+            console.log("get!!!");
+            console.log(JSON.stringify(data.data.title))
+            _this.state = {
+                'title': data.data.title,
+                'about': data.data.about,
+                'channel': data.data.channel,
+                'video': data.data.video,
+                'location': data.data.location,
+                'date_start': data.data.date_start,
+                'date_end': data.data.date_end,
+                'picture': data.data.picture,
+                'picture_large': data.data.picture_large,
+                'year_require': data.data.year_require,
+                'faculty_require': data.data.faculty_require,
+                'tags': data.data.tags,
+                'forms': data.data.forms,
+
+                'new_title': data.data.title,
+                'new_about': data.data.about,
+                'new_channel': data.data.channel,
+                'new_video': data.data.video,
+                'new_location': data.data.location,
+                'new_date_start': data.data.date_start,
+                'new_date_end': data.data.date_end,
+                'new_picture': data.data.picture,
+                'new_picture_large': data.data.picture_large,
+                'new_year_require': data.data.year_require,
+                'new_faculty_require': data.data.faculty_require,
+                'new_tags': data.data.tags,
+                'new_forms': data.data.forms,
+            }
+        }, (error) => {
+            console.log("get event error");
+        });
+
+        this.onKeyPressed = this.onKeyPressed.bind(this);
     }
 
-    render() {
+    onKeyPressed() {
+        const newState = {
+            ...this.state,
+            'new_title': this.refs.title.value,
+            'new_about': this.refs.about.value,
+            'new_channel': this.refs.channel.value,
+            'new_video': this.refs.video.value,
+            'new_location': this.refs.location.value,
+            'new_date_start': this.refs.date_start.value,
+            'new_date_end': this.refs.date_end.value,
+            'new_picture': this.refs.picture.value,
+            'new_picture_large': this.refs.picture_large.value,
+            'new_year_require': this.refs.year_require.value,
+            'new_faculty_require': this.refs.faculty_require.value,
+            'new_tags': this.refs.tags.value,
+            'new_forms': this.refs.forms.value,
+        };
+        this.setState(newState);
+    }
 
-        let eventName = (
-            <div className="event-name">
-                <h2>{this.state.title}</h2>
-                <div className="flex">
-                    <img data-icon="channel-icon" alt="channel-icon" /><span> {this.state.channel_name}</span>
-                </div>
-            </div>
-        );
+    save() {
+        const newState = {
+            ...this.state,
+            'title': this.refs.title.value,
+            'about': this.refs.about.value,
+            'channel': this.refs.channel.value,
+            'video': this.refs.video.value,
+            'location': this.refs.location.value,
+            'date_start': this.refs.date_start.value,
+            'date_end': this.refs.date_end.value,
+            'picture': this.refs.picture.value,
+            'picture_large': this.refs.picture_large.value,
+            'year_require': this.refs.year_require.value,
+            'faculty_require': this.refs.faculty_require.value,
+            'tags': this.refs.tags.value,
+            'forms': this.refs.forms.value,
+        };
+        this.setState(newState);
 
-        const isAdmin = (this.props.isAdmin) ? this.props.isAdmin : false;
-        let adminComponent = (
-            <div>
-                <div className="flex">
-                    <div className="btn-top">EDIT</div>
-                    <div className="btn-top">PARTICIPANTS LIST</div>
-                </div>
-                <p className="hr" />
-            </div>
-        )
+        let config = {
+            'headers': {
+                'Authorization': ('JWT ' + getCookie('fb_sever_token'))
+            }
+        }
 
-        let content = (
-            <div>
-                <div className="top-moving">
-                    <div className="toggle">
-                        {eventName}
-                    </div>
-                            <img className="chan-img" href="../../resource/images/poster_dummy/dummyProflie.png"/>
+        let responseBody = {
+            'title': this.refs.title.value,
+            'about': this.refs.about.value,
+            'channel': this.refs.channel.value,
+            'video': this.refs.video.value,
+            'location': this.refs.location.value,
+            'date_start': this.refs.date_start.value,
+            'date_end': this.refs.date_end.value,
+            'picture': this.refs.picture.value,
+            'picture_large': this.refs.picture_large.value,
+            'year_require': this.refs.year_require.value,
+            'faculty_require': this.refs.faculty_require.value,
+            'tags': this.refs.tags.value,
+            'forms': this.refs.forms.value,
+        }
 
+        let _this = this;
 
-                    <div className="column">
-                        <div className="toggle-not">
-                            {eventName}
-                        </div>
+        axios.put('http://128.199.208.0:1111/event?id='+ _this.state.event_id, responseBody, config).then((response) => {
+            console.log("saved!!!");
+            return true;
+        }, (error) => {
+            console.log("save error");
+            return false;
+        })
 
+        this.props.toggle_pop_item();
+    }
 
-                                <div className="to-right" >
-                                    <button alt="btn-interest">INTEREST</button>
-                                </div>
+    cancel() {
+        const newState = {
+            ...this.state,
+            'new_title': this.state.title,
+            'new_about': this.state.about,
+            'new_channel': this.state.channel,
+            'new_video': this.state.video,
+            'new_location': this.state.location,
+            'new_date_start': this.state.date_start,
+            'new_date_end': this.state.date_end,
+            'new_picture': this.state.picture,
+            'new_picture_large': this.state.picture_large,
+            'new_year_require': this.state.year_require,
+            'new_faculty_require': this.state.faculty_require,
+            'new_tags': this.state.tags,
+            'new_forms': this.state.forms,
+        };
+        this.setState(newState);
+        this.props.toggle_pop_item();
+    }
 
-                    </div>
-                </div>
-                <p className="hr"></p>
-                <div className="event-bio">
-                    <h3 className="display-none">Bio</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id ipsum orci. Aliquam erat volutpat. Sed in erat quis mi laoreet vestibulum. Fusce vitae tempor nisi. Fusce aliquam dolor mi, sit amet egestas ex imperdiet fringilla. Nullam nisl quam, convallis vitae viverra in, lacinia nec lectus. Praesent nibh metus, luctus nec tortor sit amet, hendrerit congue elit. Aliquam erat volutpat. Quisque ex arcu, iaculis blandit arcu at, molestie mattis mauris. Etiam in tortor nec metus eleifend euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </p>
-                    <p>
-                        Nulla congue tincidunt sem non ullamcorper. Etiam mattis auctor tellus. Aenean ultricies lacus at elementum fermentum. Nunc hendrerit neque et nibh sodales, a malesuada orci euismod. Aliquam a pharetra purus, nec porta augue. Donec in elit non arcu commodo tristique. Integer nunc ligula, tristique eget nisl non, consectetur pharetra lacus.
-                    </p>
-                    <p>
-                        Cras pellentesque enim justo, id pharetra neque pulvinar quis. Suspendisse a odio lacinia, maximus dolor et, maximus massa. Curabitur dolor nibh, pulvinar imperdiet nunc id, placerat venenatis massa. Donec vitae venenatis nisi. Ut iaculis sem a tempor ullamcorper. Ut aliquam venenatis fermentum. Nam porttitor libero sit amet nisl mattis, sed tincidunt eros tincidunt.
-                    </p>
-                </div>
-            </div>
-        )
-
-        if(this.state.isLoading) content = (
-            <div style={{'fontSize': '30px', 'margin': 'auto', 'color': '#878787', 'textAlign': 'center'}}>
-                <div style={{'margin': 'auto', 'width': '50px', 'display': 'inline-block', 'position': 'relative', 'top': '12px', 'marginLeft': '5px'}}>
-                    <ReactLoading type={'bars'} color={'#878787'} height='40px' width='40px' />
-                </div>
-                Loading
-                <div style={{'margin': 'auto', 'width': '50px', 'display': 'inline-block', 'position': 'relative', 'top': '12px', 'marginLeft': '5px'}}>
-                    <ReactLoading type={'bars'} color={'#878787'} height='40' width='40' />
-                </div>
-            </div>
-        );
-        else if(this.state.error) content = (
-            <div className="Warning-Container">
-                <div className="Error-Warning-Container">
-                    <div className="Error-Warning">
-                        <span>
-                            {this.state.error}
-                        </span>
-                    </div>
-                </div>
-                <button onClick={this.onExit.bind(this)}>
-                    Okay
-                </button>
-            </div>
-        )
-
+    render () {
         return (
-            <div className="modal-container">
-                <article className="event-detail-fix basic-card-no-glow">
-                    <button className="invisible square-round" role="event-exit" onClick={this.onExit.bind(this)}>
+            <div>
+                <article className="edit-event basic-card-no-glow modal-main">
+                    <button className="card-exit invisible square-round" role="event-exit" onClick={this.cancel.bind(this)}>
                         <img src="../../resource/images/X.svg" />
                     </button>
-                    {(isAdmin) ? (adminComponent) : null}
-                    {content}
+
+                    <img src="../resource/images/dummyProfile.png" className="chan-img" />
+                    
+                    <h2 className="chan-name" >CHANNEL NAME</h2>
+
+                    <p className="l1"></p>
+                     
+                    <div className="w55">
+                        <h1>DETAIL</h1> <input ref="title" type="text" placeholder="" value={this.state.new_title} onChange={this.onKeyPressed}/>
+                        <h1>PHOTO</h1> <button className="fill">UPLOAD</button>
+                        <h1>URL</h1> <input ref="location" type="text" placeholder="" value={this.state.new_location} onChange={this.onKeyPressed}/>
+                        <h1>YOUTUBE</h1> <input ref="date_start" type="text" placeholder="" value={this.state.new_date_start} onChange={this.onKeyPressed}/>
+                    </div>
+                        
+                        
+                    
+                    
+                    
+                    <div className="chan-tag">
+                        <h1>TAG</h1>
+                        <button className="tag">CAMP</button>
+                        <button className="tag">THEATRE</button>
+                        <button className="tag">TALK</button>
+                        <button className="tag">FIRSTMEET</button>
+                        <button className="tag">STAFF RECRUITMENT</button>
+                        <button className="tag">MARKET</button>
+                        <button className="tag">VOLUNTEER</button>
+                        <button className="tag">CONCERT</button>
+                        <button className="tag">FESTIVAL</button>
+                        <button className="tag">OPENING</button>
+                        <button className="tag">CONTEST</button>
+                        <button className="tag">EXHIBITION</button>
+                        <button className="tag long">WORKSHOP</button>
+                        <button className="tag">RELIGION</button>
+                        <br />
+                        <br />
+                        <button className="tag">CHARILY</button>
+                        <button className="tag long">ACADEMIC</button>
+                        <button className="tag">BUSSINESS</button>
+                        <button className="tag">CAREER</button>
+                        <button className="tag">SPORT</button>
+                        <button className="tag">ARTS</button>
+                        <button className="tag long">FOOD&DRINK</button>
+                        <button className="tag long">EDUCATION</button>
+                        <button className="tag">MUSIC</button>
+                        <button className="tag long">TECHNOLOGY</button>
+                        <button className="tag">NATURAL</button>
+                        <button className="tag">HEALTH</button>
+                    </div>
+                    
+                    <br />
+                    <br />
+                    <br />
+                    <div>
+                        <button className="bt blue" onClick={this.save.bind(this)}>SAVE</button>
+                        <button className="bt" onClick={this.cancel.bind(this)}>CANCLE</button>
+                    </div>
                 </article>
                 <div className="background-overlay"/>
             </div>
-        );
+        )
     }
 }
 
-channelInfo.defaultProps = {
-    'eventId': '594bf476e374d100140f04ec'
-}
-
-export default channelInfo;
+export default autoBind(EditEvent);
