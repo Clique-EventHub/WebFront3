999056ac7443eec0d388f78673bad97a3cbd782a
tmwtml <tangmo_wtml@hotmail.com>
2017-06-30 00:09:24 +0700

    Fix Edit Profile

--- src/container/editProfile.js
+++ src/container/editProfile.js
@@ -36,7 +36,8 @@ class editProfile extends Component {
                 'firstName': data.data.firstName,
                 'lastName': data.data.lastName,
                 'picture': data.data.picture_200px,
-                'regId': data.data.regId,
+                'regId': (data.data.regId == null) ? 'undefined' : data.data.regId,
+                'faculty': (data.data.faculty == null)? 'Faculty of Engineering': data.data.faculty,
                 'birth_day': data.data.birth_day,
                 'nick_name': data.data.nick_name,
                 'lineId': data.data.lineId,
@@ -45,8 +46,10 @@ class editProfile extends Component {
                 'shirt_size': data.data.shirt_size,
                 'disease': data.data.disease,
                 'allergy': data.data.allergy,
+                'dorm_building': data.data.dorm_building,
+                'dorm_room': data.data.dorm_room,
+                'dorm_bed': data.data.dorm_bed,
 
-                'new_regId': data.data.regId,
                 'new_birth_day': data.data.birth_day,
                 'new_nick_name': data.data.nick_name,
                 'new_lineId': data.data.lineId,
@@ -55,6 +58,9 @@ class editProfile extends Component {
                 'new_shirt_size': data.data.shirt_size,
                 'new_disease': data.data.disease,
                 'new_allergy': data.data.allergy,
+                'new_dorm_building': data.data.dorm_building,
+                'new_dorm_room': data.data.dorm_room,
+                'new_dorm_bed': data.data.dorm_bed,
                 'isLoading': false
             })
         }, (error) => {
@@ -65,7 +71,6 @@ class editProfile extends Component {
     onKeyPressed() {
         const newState = {
             ...this.state,
-            'new_regId': this.refs.id.value,
             'new_birth_day': this.refs.birth.value,
             'new_nick_name': this.refs.nick_name.value,
             'new_lineId': this.refs.line.value,
@@ -74,6 +79,9 @@ class editProfile extends Component {
             'new_shirt_size': this.refs.size.value,
             'new_disease': this.refs.med.value,
             'new_allergy': this.refs.food.value,
+            'new_dorm_building': this.refs.dorm_building.value,
+            'new_dorm_room': this.refs.dorm_room.value,
+            'new_dorm_bed': this.refs.dorm_bed.value,
         };
         this.setState(newState);
     }
@@ -81,7 +89,6 @@ class editProfile extends Component {
     save() {
         const newState = {
             ...this.state,
-            'regId': this.refs.id.value,
             'birth_day': this.refs.birth.value,
             'nick_name': this.refs.nick_name.value,
             'lineId': this.refs.line.value,
@@ -90,6 +97,9 @@ class editProfile extends Component {
             'shirt_size': this.refs.size.value,
             'disease': this.refs.med.value,
             'allergy': this.refs.food.value,
+            'dorm_building': this.refs.dorm_building.value,
+            'dorm_room': this.refs.dorm_room.value,
+            'dorm_bed': this.refs.dorm_bed.value,
         };
         this.setState(newState);
 
@@ -100,7 +110,6 @@ class editProfile extends Component {
         }
 
         let responseBody = {
-            'regId': this.refs.id.value,
             'birth_day': this.refs.birth.value,
             'nick_name': this.refs.nick_name.value,
             'lineId': this.refs.line.value,
@@ -109,6 +118,9 @@ class editProfile extends Component {
             'shirt_size': this.refs.size.value,
             'disease': this.refs.med.value,
             'allergy': this.refs.food.value,
+            'dorm_building': this.refs.dorm_building.value,
+            'dorm_room': this.refs.dorm_room.value,
+            'dorm_bed': this.refs.dorm_bed.value,
         }
 
         axios.put('http://128.199.208.0:1111/user', responseBody, config).then((response) => {
@@ -128,7 +140,6 @@ class editProfile extends Component {
     cancel() {
         const newState = {
             ...this.state,
-            'new_regId': this.state.regId,
             'new_birth_day': this.state.birth_day,
             'new_nick_name': this.state.nick_name,
             'new_lineId': this.state.lineId,
@@ -137,6 +148,9 @@ class editProfile extends Component {
             'new_shirt_size': this.state.shirt_size,
             'new_disease': this.state.disease,
             'new_allergy': this.state.allergy,
+            'new_dorm_building': this.state.dorm_building,
+            'new_dorm_room': this.state.dorm_room,
+            'new_dorm_bed': this.state.dorm_bed,
         };
         this.setState(newState);
         this.props.toggle_pop_item();
@@ -165,7 +179,7 @@ class editProfile extends Component {
                         <p className="hr"></p>
                         <div className="flex">
                             <section className="edit-pro-left">
-                                <img src="../../resource/icon/icon3.svg" alt="id"/> <input ref="id" type="text" placeholder="Student ID" value={this.state.new_regId} onChange={this.onKeyPressed}/>
+                                <img src="../../resource/icon/icon3.svg" alt="id"/> <p>{this.state.regId}</p>
                                 <img src="../../resource/icon/icon6.svg" alt="birth"/> <input ref="birth" type="text" placeholder="Birthdate" value={this.state.new_birth_day} onChange={this.onKeyPressed}/>
                                 <img src="../../resource/icon/icon2.svg" alt="nickname"/> <input ref="nick_name" type="text" placeholder="Nickname" value={this.state.new_nick_name} onChange={this.onKeyPressed}/>
                                 <img src="../../resource/icon/icon8.svg" alt="line"/> <input ref="line" type="text" placeholder="Line ID" value={this.state.new_lineId} onChange={this.onKeyPressed}/>
@@ -174,6 +188,12 @@ class editProfile extends Component {
                                 <img src="../../resource/icon/icon11.svg" alt="size"/> <input ref="size" type="text" placeholder="T-Shirt Size" value={this.state.new_shirt_size} onChange={this.onKeyPressed}/>
                                 <img src="../../resource/icon/icon12.svg" alt="med"/> <input ref="med" type="text" placeholder="Medical Problem" value={this.state.new_disease} onChange={this.onKeyPressed}/>
                                 <img src="../../resource/icon/icon13.svg" alt="food"/> <input ref="food" type="text" placeholder="Food Allergy" value={this.state.new_allergy} onChange={this.onKeyPressed}/>
+                                <img src="../../resource/icon/icon14.svg" alt="dorm"/>
+                                <div className="flex">
+                                    <input className="w100" ref="dorm_building" type="text" placeholder="Building" value={this.state.new_dorm_building} onChange={this.onKeyPressed}/>
+                                    <input className="w50" ref="dorm_room" type="text" placeholder="Room" value={this.state.new_dorm_room} onChange={this.onKeyPressed}/>
+                                    <input className="w50" ref="dorm_bed" type="text" placeholder="Bed" value={this.state.new_dorm_bed} onChange={this.onKeyPressed}/>
+                                </div>
                             </section>
                             <p className="sec-line"></p>
                             <section className="edit-pro-right">
