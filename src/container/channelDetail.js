6f1470d3b507e4aef6be674e8ed071dff5014380
FM <ekkalakfm@gmail.com>
2017-07-04 00:06:58 +0700

    Fetch data from server

--- src/container/channelDetail.js
+++ src/container/channelDetail.js
@@ -20,21 +20,22 @@ class channelInfo extends Component {
         // about, video, location, date_start, date_end, picture, picture_large, year_require, faculty_require, tags, agreement, contact_information,
         // joinable_start_time, joinable_end_time, joinable_amount, time_start, time_end, optional_field, require_field, show, outsider_accessible
 
-        let _this = this;
 
         this.state = {
-            'channel_id': "5946205a4b908f001403aba5"
+            'name': "",
+            'picture': "",
+            'detail': "",
+            'picture_large': []
         }
-
-        axios.get('http://128.199.208.0:1111/channel?id=' + _this.state.channel_id).then((data) => {
+        axios.get('http://128.199.208.0:1111/channel?id=' + "5953e2f4dd3c09001422e9ed").then((data) => {
             console.log("get!!!dd");
             console.log(JSON.stringify(data.data.name))
-            _this.state = {
+            this.setState({
                 'name': data.data.name,
                 'picture': data.data.picture,
                 'detail': data.data.detail,
-                'picture_large': data.data.picture_large,
-            }
+                'picture_large': data.data.picture_large
+            });
         }, (error) => {
             console.log("get channel error");
         });
@@ -45,6 +46,7 @@ class channelInfo extends Component {
     }
 
     render () {
+console.log(this.state.detail);
         return (
             <div>
                 <article className="edit-event basic-card-no-glow modal-main">
@@ -66,9 +68,13 @@ class channelInfo extends Component {
                     </div>
                 <div className="marginleft">
                     <div className="chan-img-slide">
-                        {this.state.picture_large.map((url) => {
-                            return <a href={url}><img src={url} /></a>
-                        })}
+                        {() => {
+                            if(this.state.picture_large.length > 0) {
+                                this.state.picture_large.map((url) => {
+                                    return <a href={url}><img src={url} /></a>
+                                })
+                            }
+                        }}
                     </div>
                     <a href="#" className="box">
                         FACEBOOK
